dbeba1c81191081338d4f0ebdd4aa6d936f971ae
Manja Wilson <manjafiebig@aol.com>
2023-06-30 16:30:33 -0400

    register and login

--- webapp.py
+++ webapp.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'b5a12cddfa1f550de97ff444'
 
 posts = [
     { 'author' : 'John Doe',
@@ -27,6 +30,26 @@ def home():
 def about():
     return render_template('about.html', title='About' )
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login Unsuccessfull. Please check username and password!', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 
